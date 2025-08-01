9c6e9aee72d835ad2a02d50d95a50f11b240e591
Robinaditya1045 <mahamuniaditya07@gmail.com>
2025-08-02 01:01:16 +0530

    init

---
+++ client/hardhat.config.js
@@ -0,0 +1,21 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("hardhat/config");
+
+const path = require('path');
+
+module.exports = {
+  solidity: "0.8.18",
+  paths: {
+    sources: "./",  // Contract source directory
+    tests: "./test",         // Test files directory
+    cache: "./cache",        // Cache directory
+    artifacts: "./artifacts" // Compiled artifacts directory
+  },
+  networks: {
+    hardhat: {},
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+  }
+};
